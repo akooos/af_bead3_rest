34832ca4838e70e4795ecbccf5f67e2f2e8899fb
Akos Toth <zuiadaton@gmail.com>
2016-01-22 09:34:19 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,64 @@
+# Követelményanalízis
+
+## Funkcionális követelmények
+
+- REST szerver
+
+## Nem funkcionális követelmények
+
+- Biztonságos működés: jelszavak tárolása, funkciókhoz való hozzáférés
+- Gyors
+
+### Végpontok
+
+| Kérés| Útvonal           | Leírás                                           |
+|------|-------------------|--------------------------------------------------|
+| GET  | /                 | metaadatok                                       |
+| GET  | /users            | felhasználól                                     |
+| GET  | /classes         |  tantárgyak                                       |
+
+# Implementáció
+
+##Fejlesztői környezet bemutatása
+
+###Könyvtárstruktúrában lévő mappák és fájlok funkiójának bemutatása:
+
+| Útvonal                  | Leírás                                            |
+|--------------------------|---------------------------------------------------|
+| /.db                     | adatok tárhelye                                   |
+
+
+# Felhasználói dokumentáció
+
+## A futtatáshoz ajánlott hardver-, szoftver konfiguráció
+
+###Server oldalon: 
+
+- Op. rendszer: Windows/Linux...(Linux ajánlot)
+- NodeJS
+
+###Kliens oldalon: 
+- Tetszőleges HTTP/1.1 kliens, amely képes JSON formátumú adatokat fogadni
+
+##Telepítés lépései
+
+###Linux-on
+
+####Következő csomagok(programok) szükségesek: `nodejs`
+
+`cd /installation/path`
+
+`git clone <git repository url>`
+
+`npm install express –save`
+
+`npm install fortune --save`
+
+`npm install fortune-json-api --save`
+
+`npm install fortune-nedb --save`
+
+
+####Szerver indítása:
+
+`node server.js`
---
+++ server.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var fortune = require('fortune');
+var nedbAdapter = require('fortune-nedb');
+var jsonapi = require('fortune-json-api');
+
+
+
+// Beállítjuk a JSON API serializer-t
+var store  = fortune({
+    adapter: {
+        type: nedbAdapter,
+        options: { dbPath: __dirname + '/.db' }
+    },
+    serializers: [{ type: jsonapi }]    
+});
+var server = express();
+// Express middleware
+
+// Minden URL-ről engedélyezzük a hozzáférést az API-hoz
+// Mindenképp a `server.use(fortune.net.http(store));` sor elé kerüljön
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+                    
+    next();
+});
+server.use("/public", express.static(__dirname+ 'public'));
+server.use(fortune.net.http(store));
+store.defineType('class', {
+    name: {type: String},
+    dayOfWeek: {type: Number},
+    hour: {type: Number},
+    length: {type: Number},
+    user: { 
+        link: 'user',
+        inverse: 'class',
+        isArray: false
+    }
+});
+
+store.defineType('user', {
+    name: {type: String},
+    username:{type: String},
+    password:{type: String},
+    surname:{type: String},
+    forename:{type: String},
+    email:{type: String},
+    "class": { 
+        link: 'class',
+        inverse: 'user',
+        isArray: true
+    }
+});
+
+// Csak akkor fusson a szerver, ha sikerült csatlakozni a tárolóhoz
+// Hasonlóan a Waterline-hoz
+var port = process.env.PORT || 8080;
+store.connect().then(function () {
+    server.listen(port, function () {
+        console.log('JSON Api server started on port ' + port);
+    });
+});
+
+
+/*
+
+var express = require('express');
+var fortune = require('fortune');
+var nedbAdapter = require('fortune-nedb');
+var jsonapi = require('fortune-json-api');
+// Új tároló (alapértelmezetten memóriában tárol)
+var store = fortune({
+    
+    adapter:{
+        type:nedbAdapter,
+        options:{dbPath: __dirname + '/db'}
+        
+         
+    },
+    
+    serializers: [{ type: jsonapi }]   
+    
+    
+});
+
+var server = express();
+// Express middleware
+server.use(fortune.net.http(store));
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});*/
+/*store.defineType('person',{
+    fname:{type:String},
+    lname:{type:String},
+    birthday:{type:Date}
+    
+});
+*//*
+store.defineType('error',{
+    location:{type:String},
+    description:{type:String},
+    date:{type:String},
+    status:{type:String}
+     
+});
+
+
+store.connect().then(function () {
+    var port = process.env.PORT || 8080;
+    server.listen(port, function () {
+        console.log('JSON Api server started on port ' + port);
+    });
+});*/
